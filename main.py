bd0e0e22865c0c512cd7d87d89323ac042465e3c
Derian8 <dacunar@ctpplatanares.ed.cr>
2025-08-22 16:17:36 -0600

    proyectos, modulos
    Derian

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-# Proyecto: [Nombre del Proyecto]
-# Estudiante: [Nombre del Estudiante]
-# Fecha de Inicio: [dd/mm/aaaa]
-# Fecha de Entrega: [dd/mm/aaaa]
+# Proyecto: [Modulos y Funciones Python]
+# Estudiante: [Derian Isaac Acuña Rojas]
+# Fecha de Inicio: [22/8/2025]
+# Fecha de Entrega: [22/9/2025]
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
-# Esto ayuda a mantener un registro claro del trabajo realizado.
+# Esto ayuda a mantener un registro claro del trabajo realizado
+
+#
+from analisis_datos import *
+
+def saludar():
+    print ("Hola desde la funcion")
+    
+if __name__== "__main__":
+    compras = generar_lista_compras()
+    print(compras)
+    media = media (list(compras.values()))
+    print( "promedio de costo por articulo : ", media)
+    
+    mediana = mediana(list(compras.values()))
+    print("mediana de costo por articulo :", mediana)
+  
